18cb2d55256eee8c6fc6f2c205740433dc802073
Codibex <Michael_H@live.at>
2024-09-23 09:30:11 +0000

    Add certificate direct to request

--- homeassistant/components/savvy/stock_api.py
+++ homeassistant/components/savvy/stock_api.py
@@ -1,5 +1,8 @@
 """Provides an API wrapper for Savvy to fetch product stocks."""
 
+import os
+import ssl
+
 import aiohttp
 
 
@@ -10,12 +13,26 @@ class StockApi:
         """Initialize the StockApi with the base URL."""
 
         self.base_url = base_url
+        self.cert_path = os.path.join(
+            os.path.dirname(__file__), "savvy_certificate.crt"
+        )
+
+    def _create_ssl_context(self):
+        """Create an SSL context if a certificate path is provided."""
+        if self.cert_path:
+            ssl_context = ssl.create_default_context(cafile=self.cert_path)
+        else:
+            ssl_context = None
+        return ssl_context
 
     async def fetch_product_stocks(self) -> list:
         """Fetch product stocks from the Savvy API."""
 
+        ssl_context = self._create_ssl_context()
+        conn = aiohttp.TCPConnector(ssl=ssl_context)
+
         async with (
-            aiohttp.ClientSession() as session,
+            aiohttp.ClientSession(connector=conn) as session,
             session.get(f"{self.base_url}/api/stock-mgmt/stock/overview") as response,
         ):
             return await response.json()
@@ -23,7 +40,10 @@ class StockApi:
     async def async_update_stock(self, product_id, adjustmentType, amount):
         """Update stock for a product."""
 
-        async with aiohttp.ClientSession() as session:
+        ssl_context = self._create_ssl_context()
+        conn = aiohttp.TCPConnector(ssl=ssl_context)
+
+        async with aiohttp.ClientSession(connector=conn) as session:
             await session.patch(
                 f"{self.base_url}/api/stock-mgmt/stock/products/{product_id}/adjust",
                 json={"adjustmentType": adjustmentType, "amount": amount},
